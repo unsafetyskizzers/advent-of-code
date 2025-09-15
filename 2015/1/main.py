553cff6be18d41e59ef7901c070234df5d39397d
unsafetyskizzers <camden44mason@gmail.com>
2025-09-14 22:22:20 -0500

    day 1 part 2 complete

--- 2015/1/main.py
+++ 2015/1/main.py
@@ -1,11 +1,15 @@
 code_file = open("code.txt", "r")
 code_text = code_file.read()
+code_file.close()
 current_floor = 0
+basement_index = -1
 
-for i in code_text:
-    if i == "(": current_floor += 1
-    elif i == ")": current_floor -= 1
+for i in range(len(code_text)):
+    if code_text[i] == "(": current_floor += 1
+    elif code_text[i] == ")": current_floor -= 1
+    if current_floor < 0 and basement_index == -1: basement_index = i + 1
     
-print(current_floor)
+print("Floors travelled:", current_floor)
+print("First basement visit:", basement_index)
 
 input()
