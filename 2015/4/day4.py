ee55103f395d0e33f6166114e1c8469efc842359
unsafetyskizzers <camden44mason@gmail.com>
2025-09-16 23:32:31 -0500

    2015: day 4 part 2 complete

--- 2015/4/day4.py
+++ 2015/4/day4.py
@@ -12,6 +12,18 @@ while answer_hash.hexdigest()[:5] != "00000":
     hash_inc += 1
     answer_hash = hashlib.md5((input_code + str(hash_inc)).encode("utf-8"))
 if answer_hash.hexdigest()[:5] == "00000":
-    print("ANSWER FOUND:", hash_inc)
+    print("ANSWER FOUND for 5 zeroes:", hash_inc)
 else:
-    print("No answer found")
+    print("No answer found for 5 zeroes")
+
+#start over for part 2
+answer_hash = hashlib.md5()
+hash_inc = -1
+
+while answer_hash.hexdigest()[:6] != "000000":
+    hash_inc += 1
+    answer_hash = hashlib.md5((input_code + str(hash_inc)).encode("utf-8"))
+if answer_hash.hexdigest()[:6] == "000000":
+    print("ANSWER FOUND for 6 zeroes:", hash_inc)
+else:
+    print("No answer found for 6 zeroes")
