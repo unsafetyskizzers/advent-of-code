8329bf107fc103aaa746b865afe43b705d061ad1
unsafetyskizzers <camden44mason@gmail.com>
2025-09-16 23:28:36 -0500

    2015: day 4 part 1 complete

---
+++ 2015/4/day4.py
@@ -0,0 +1,17 @@
+import hashlib
+file = open("input.txt", "r")
+
+answer_hash = hashlib.md5()
+hash_inc = -1
+input_code = file.read()
+file.close()
+
+print(input_code)
+
+while answer_hash.hexdigest()[:5] != "00000":
+    hash_inc += 1
+    answer_hash = hashlib.md5((input_code + str(hash_inc)).encode("utf-8"))
+if answer_hash.hexdigest()[:5] == "00000":
+    print("ANSWER FOUND:", hash_inc)
+else:
+    print("No answer found")
