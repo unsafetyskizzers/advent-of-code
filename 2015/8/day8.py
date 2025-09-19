bf15bc4b0eaf207fb12f8900a652b5e066a94258
unsafetyskizzers <camden44mason@gmail.com>
2025-09-19 13:34:02 -0500

    2015: day 8-1 complete

---
+++ 2015/8/day8.py
@@ -0,0 +1,29 @@
+input = open("2015/8/input.txt", "r")
+lines = input.readlines()
+input.close()
+
+for i in range(len(lines)):
+    lines[i] = lines[i].strip()
+
+code_chars = 0
+str_chars = 0
+diff_chars = 0
+
+for line in lines:
+    code_chars = 0
+    str_chars = 0
+    skip_chars = 0
+    code_chars += len(line)
+    for i in range(len(line)):
+        if skip_chars == 0:
+            if line[i] == "\\":
+                if line[i+1] == "\\" or line[i+1] == "\"":
+                    skip_chars = 1
+                elif line[i+1] == "x":
+                    skip_chars = 3
+            str_chars += 1
+        else:
+            skip_chars -= 1
+    str_chars -= 2
+    diff_chars += (code_chars - str_chars)
+print("Difference:", diff_chars)
