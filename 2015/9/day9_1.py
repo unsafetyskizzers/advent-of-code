111d4c752d8143c12951178cb5da74acffd26f71
unsafetyskizzers <camden44mason@gmail.com>
2025-09-19 15:22:40 -0500

    2015: day 9-1 complete

---
+++ 2015/9/day9_1.py
@@ -0,0 +1,44 @@
+file = open("2015/9/input.txt", "r")
+input = file.readlines()
+file.close()
+
+edges = {}
+
+for line in input:
+    command = line.split(" ")
+    if command[0] not in edges:
+        edges[command[0]] = {}
+    if command[2] not in edges:
+        edges[command[2]] = {}
+    if command[2] not in edges[command[0]]:
+        edges[command[0]][command[2]] = int(command[4])
+    if command[0] not in edges[command[2]]:
+        edges[command[2]][command[0]] = int(command[4])
+
+locations = edges.keys()
+possible_routes = []
+shortest_route = []
+shortest_route_length = 999999
+
+def all_subroutes(curr_route, full_route):
+    if len(curr_route) != len(full_route):
+        for dest in full_route:
+            if dest not in curr_route:
+                new_curr_route = curr_route.copy()
+                new_curr_route.append(dest)
+                all_subroutes(new_curr_route, full_route)
+    else:
+        possible_routes.append(curr_route)
+
+for loc in locations:
+    all_subroutes([loc], locations)
+
+for route in possible_routes:
+    route_length = 0
+    for i in range(len(route)-1): 
+        route_length += edges[route[i]][route[i+1]]
+    if route_length < shortest_route_length:
+        shortest_route_length = route_length
+        shortest_route = route
+
+print("Shortest route:", shortest_route, "of length", shortest_route_length)
