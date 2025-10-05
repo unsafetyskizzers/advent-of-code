28d2c8019ab4a81417659a7d9a314be674852e9c
unsafetyskizzers <camden44mason@gmail.com>
2025-10-05 12:29:01 -0500

    2015: day 19 part 2 will probably do the trick eventually

--- 2015/19/day19.py
+++ 2015/19/day19.py
@@ -33,25 +33,45 @@ for key in replacements.keys():
 
 print(len(new_molecules))
 
-molecule_tree = ["e"]
+molecule_tree = []
+molecule_tree.append(medicine)
 new_molecule_tree = []
 generation = 0
 
+file = open("2015/19/input.txt", "r")
+input = file.readlines()
+file.close()
+
+reverse_replacements = {}
+
+for line in input:
+    line_str = line.strip()
+    if line_str == "":
+        break
+    command = line_str.split()
+    if command[2] in reverse_replacements:
+        reverse_replacements[command[2]].append(command[0])
+    else:
+        reverse_replacements[command[2]] = [command[0]]
+
+print(reverse_replacements)
+
 # NOTE: work BACKWARDS from finished molecule!!
-while medicine not in molecule_tree:
+while "e" not in molecule_tree:
     new_molecule_tree = []
     generation += 1
     for mol in molecule_tree:
-        for key in replacements.keys():
+        for key in reverse_replacements.keys():
             new_molecule = ""
             if key in mol:
                 for i in range(len(medicine) - len(key) + 1):
                     if mol[i:i+len(key)] == key:
-                        for rep in replacements[key]:
+                        for rep in reverse_replacements[key]:
                             new_molecule = mol[0:i] + rep + mol[i+len(key):]
                             if new_molecule not in new_molecules:
                                 new_molecule_tree.append(new_molecule)
                                 print(new_molecule)
     molecule_tree = new_molecule_tree
+    print("gen", generation)
 
 print("Medicine found in", generation, "steps")
